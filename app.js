aecdbe76e66c03d4b4a406e20535aeacc4932e0f
Kriston Luces <95931845+KristonLuces@users.noreply.github.com>
2022-01-08 16:14:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
 
 
 //Display Mobile Menu
@@ -9,3 +10,15 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click',mobileMenu);
+
+//Close mobile nav
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu)
+navLogo.addEventListener('click', hideMobileMenu)
