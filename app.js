5332c7602f6724046b317598a39f59c691d18da5
Kriston Luces <95931845+KristonLuces@users.noreply.github.com>
2022-01-08 21:35:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,4 +21,4 @@ const hideMobileMenu = () => {
 }
 
 menuLinks.addEventListener('click', hideMobileMenu)
-navLogo.addEventListener('click', hideMobileMenu)
+//navLogo.addEventListener('click', hideMobileMenu)
